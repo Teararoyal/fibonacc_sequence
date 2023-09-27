4c061da808121e364ad813d62f67eaffd1700a82
15526627 <nattanan_nick@live.com>
2023-09-27 22:08:04 +0700

    finish

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,18 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getFibonacciSequence(countingLength: number): any {
-    
-    const sequence = [0, 1];
-    let total = 1;
-    for (let i = 2; i < countingLength; i++) {
+  getFibonacciSequence(memberCount: number): any {
+    const sequence = [1];
+    let total= 1;
+
+    if (memberCount > 1) {
+      sequence.push(1);
+      total = 2;
+    }
+
+    for (let i = 2; i < memberCount; i++) {
       const nextValue = sequence[i - 1] + sequence[i - 2];
       sequence.push(nextValue);
       total += nextValue;
     }
 
     return {
-      'member-count': countingLength,
+      'member-count': memberCount,
       sequence,
       total,
     };
